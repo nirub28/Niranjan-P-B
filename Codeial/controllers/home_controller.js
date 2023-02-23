af63c2ebfa0bd3a306479bc970199f0aba122d4e
Niranjan P B <niranjanbgowda123@gmail.com>
2023-02-23 23:52:17 +0530

    mader async await

--- Codeial/controllers/home_controller.js
+++ Codeial/controllers/home_controller.js
@@ -1,29 +1,32 @@
 const Post=require('../models/Post');
 const User=require('../models/User');
 
-module.exports.home=function(req,res){
+module.exports.home= async function(req,res){
    // to get object of user and pass it so we can access name of user in display
-   Post.find({})
+
+   try{
+
+      let posts = await Post.find({})
    .populate('user')
    .populate({
       path:'comments',
       populate:{
          path:'user'
       }
-   })
-   .exec( function(err, posts){
-      //console.log(posts);
-     // //if(err){console.log('error in extracting posts');return;}
-      User.find({}, function(err,users){
-         return res.render('home',{
-            title:"Codeial | Home",
-            posts: posts,
-            all_users: users
-      });
-     
-      });
+   });   
+    let users=await User.find({});
+        
+    return res.render('home',{
+      title:"Codeial | Home",
+      posts: posts,
+      all_users: users
+    });
+}    
 
-   })
-         
-}
+
+catch(err){
+          console.log('Error', err);
+          return;
+   }
+}   
 //can create function with above syntax an export to use in routes
--- Codeial/controllers/post_controller.js
+++ Codeial/controllers/post_controller.js
@@ -1,29 +1,39 @@
-const Post=require('../models/Post');
+const Post = require('../models/Post');
 //as we need to delete comments associated with with post, we import it
-const Comment =require('../models/Comment');
+const Comment = require('../models/Comment');
 
-module.exports.create=function(req,res){
-    Post.create({
-        content : req.body.content,
-        user : req.user._id
-    },function(err,post){
-          if(err){console.log("Error is storing post"); return;}
-                
-          return res.redirect('back');
-    });
+module.exports.create = function (req, res) {
+  Post.create({
+    content: req.body.content,
+    user: req.user._id
+  }, function (err, post) {
+    if (err) { console.log("Error is storing post"); return; }
+
+    return res.redirect('back');
+  });
 }
 
-module.exports.destroy=function(req,res){
-    //.id means converting object id to string
-    Post.findById(req.params.id, function(err,post){
-        if(post.user==req.user.id){ //to check if logged in user and post user is same
-          post.remove();
-          Comment.deleteMany({post:req.params.id},function(err){//finding the post id and deleteting commets of it
-            return res.redirect('back');
-          });   
-        }
-        else{
-            return res.redirect('back');
-          }
-    });
+module.exports.destroy = async function (req, res) {
+  //.id means converting object id to string
+
+  try{
+
+    let post = await Post.findById(req.params.id);
+
+  if (post.user == req.user.id) { //to check if logged in user and post user is same
+    post.remove();
+
+    await Comment.deleteMany({ post: req.params.id });//finding the post id and deleteting commets of it
+    return res.redirect('back');
+  }
+  else {
+    return res.redirect('back');
+  }
+
+  }catch(err){
+    console.log('Error', err);
+    return;
+  }
+  
+
 }
